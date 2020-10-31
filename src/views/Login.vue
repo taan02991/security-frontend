ed239747b785d0a42bf7b2a5900734bd7c65e2c0
Thanawat <taan02991@hotmail.com>
2020-11-01 00:50:18 +0700

    Add Login Page and Navbar

---
+++ src/views/Login.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-container fill-height fluid>
+    <v-row justify="center" align="center">
+      <v-col cols="12" sm="8" md="4">
+        <v-card>
+        <v-card-text>
+            <div class="text-h3">Login</div>
+          </v-card-text>
+          <v-card-text>
+            <v-form>
+              <v-text-field
+                v-model="username"
+                label="Username"
+                placeholder="Username"
+                outlined
+              ></v-text-field>
+              <v-text-field
+                v-model="password"
+                label="Password"
+                placeholder="Password"
+                type="password"
+                outlined
+              ></v-text-field>
+            </v-form>
+            <v-btn
+              @click="logInButtonHandler"
+              class="my-2"
+              x-large
+              color="success"
+              dark
+              block
+              >Login</v-btn
+            >
+            <v-btn
+              @click="$router.push('/register')"
+              x-large
+              color="primary"
+              dark
+              block
+              >Register</v-btn
+            >
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-property-decorator'
+import Component from 'vue-class-component'
+
+@Component
+export default class Login extends Vue {
+  private username = '';
+  private password = '';
+}
+</script>
