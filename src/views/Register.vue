23d0e3b120992cf94199593f8e2c9877c8f96fee
Thanawat <taan02991@hotmail.com>
2020-11-01 01:16:55 +0700

    Add Register Page

---
+++ src/views/Register.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-container fill-height fluid>
+    <v-row justify="center" align="center">
+      <v-col cols="12" sm="8" md="4">
+        <v-card>
+          <v-card-text>
+            <div class="text-h3">Register</div>
+          </v-card-text>
+          <v-card-text>
+            <v-form>
+              <v-text-field
+                v-model="username"
+                label="Username"
+                placeholder="Username"
+                outlined
+              ></v-text-field>
+              <v-text-field
+                v-model="password"
+                label="Password"
+                type="password"
+                placeholder="Password"
+                outlined
+              ></v-text-field>
+              <v-text-field
+                v-model="password2"
+                label="Confirm Password"
+                type="password"
+                placeholder="Confirm Password"
+                outlined
+              ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <div class="d-flex justify-center my-2">
+            <v-container fluid>
+              <v-btn x-large color="primary" dark block
+                >Register</v-btn
+              >
+            </v-container>
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-property-decorator'
+import Component from 'vue-class-component'
+
+@Component
+export default class Register extends Vue {
+  private username = '';
+  private password = '';
+  private password2 = '';
+}
+</script>
